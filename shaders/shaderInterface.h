50bb2d4dc7dedf0a95bbd5b0da1c9cd75c3b4786
Raven33 <8183672+Raven33@users.noreply.github.com>
2025-02-26 07:20:53 -0500

    Added UBOS default values and UI display functions

--- shaders/shaderInterface.h
+++ shaders/shaderInterface.h
@@ -1,5 +1,6 @@
 #ifndef SHADERINTERFACE_H
 #define SHADERINTERFACE_H
+#include "imgui.h"
 #ifdef __cplusplus
 #include <vulkan/vulkan.hpp>
 #include "vkHelper.hpp"
@@ -182,11 +183,13 @@ using uint = uint32;
 #ifndef __cplusplus
 #define PushConstantStruct layout(push_constant) uniform
 #define UBOStruct(ALIGNEMENT, SET, BINDING) layout(ALIGNEMENT, set = SET, binding = BINDING) uniform
-#define UBOName(NAME) NAME
+#define UBOName(NAME) NAME // we declare at the same time
+#define UBODefaultVal(V) // we can't have default values in GLSL
 #else
 #define PushConstantStruct struct
 #define UBOStruct(ALIGNEMENT, SET, BINDING) struct
-#define UBOName(NAME)
+#define UBOName(NAME) // we dont want globals in cpu
+#define UBODefaultVal(V) = V // default value for UBOs
 #endif
 
 PushConstantStruct PushConstants {
@@ -202,101 +205,266 @@ UBOStruct(scalar, SceneSet, U_viewBinding) ViewUBO {
 }UBOName(viewUbo);
 
 UBOStruct(scalar, SceneSet, U_globalShadingBinding) GlobalShadingUBO {
-    bool filmic;
-    vec3 lightPos;
-    vec3 lightColor;
-    vec3 viewPos;
-    bool linkLight;
-    bool shadingHack;
+    bool filmic UBODefaultVal(true);
+    vec3 lightPos UBODefaultVal(vec3(0));
+    vec3 lightColor UBODefaultVal(vec3(1));
+    vec3 viewPos UBODefaultVal(vec3(0));
+    bool linkLight UBODefaultVal(true);
+    bool shadingHack UBODefaultVal(true);
+#ifdef __cplusplus
+    void displayUI() {
+        if (ImGui::CollapsingHeader("Global Shading UBO")) {
+            ImGui::Text("Global Shading UBO");
+            ImGui::Checkbox("Filmic", &filmic);
+            ImGui::Checkbox("COS Lift", &shadingHack);
+            ImGui::Checkbox("Link Light", &linkLight);
+            ImGui::ColorEdit3("Light Color", &lightColor[0]);
+            ImGui::DragFloat3("Light Position", &lightPos[0], -10, 10);
+            ImGui::DragFloat3("View Position", &viewPos[0], -10, 10);
+        }
+    }
+#endif
 }UBOName(globalShadingUbo);
 
 UBOStruct(scalar, SceneSet, U_shadingBinding) ShadingUBO {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float shininess;
-    float specularStrength;
-    bool doAo;
-    bool showElementTexture;
-    bool doShading;
-    bool displayNormals;
-    bool displayPrimId;
-    int colorMode;
-    bool displayLocalUv;
-    bool displayGlobalUv;
+    vec3 ambient UBODefaultVal(vec3(0));
+    vec3 diffuse UBODefaultVal(vec3(1));
+    vec3 specular UBODefaultVal(vec3(1));
+    float shininess UBODefaultVal(32.f);
+    float specularStrength UBODefaultVal(1.f);
+    bool doAo UBODefaultVal(false);
+    bool showElementTexture UBODefaultVal(false);
+    bool doShading UBODefaultVal(true);
+    bool displayNormals UBODefaultVal(false);
+    bool displayPrimId UBODefaultVal(false);
+    int colorMode UBODefaultVal(2);
+    bool displayLocalUv UBODefaultVal(false);
+    bool displayGlobalUv UBODefaultVal(false);
+#ifdef __cplusplus
+    void displayUI(std::string meshName = "") {
+        float multiplier = 1.0f;
+        if (ImGui::CollapsingHeader(("Shading UBO " + meshName).c_str())) {
+            ImGui::PushItemWidth(100.0f);
+            ImGui::ColorEdit3("Ambient", &ambient[0]);
+            ImGui::ColorEdit3("Diffuse", &diffuse[0]);
+            ImGui::SliderFloat("Shininess", &shininess, 1, 2048, "%.1f", ImGuiSliderFlags_Logarithmic);
+            ImGui::SliderFloat("Specular Strength", &specularStrength, 0, 1000, "%.1f", ImGuiSliderFlags_Logarithmic);
+            ImGui::PopItemWidth();
+            
+            ImGui::Checkbox("Show Element Texture", &showElementTexture);
+            ImGui::Checkbox("Do Shading", &doShading);
+            ImGui::Checkbox("Display Normals", &displayNormals);
+            ImGui::Checkbox("Display Prim Id", &displayPrimId);
+            ImGui::SameLine();
+            ImGui::PushItemWidth(150.0f);
+            ImGui::Combo("Color Mode", reinterpret_cast<int *>(&colorMode), "Color Per Task\0Color Per Mesh\0Color Per Primitive\0");
+            ImGui::PopItemWidth();
+            ImGui::Checkbox("Display Local UV", &displayLocalUv);
+            ImGui::Checkbox("Display Global UV", &displayGlobalUv);
+        }
+    }
+#endif
 }UBOName(shadingUbo);
 
 #ifdef HE_PIPELINE
 UBOStruct(scalar, OtherSet, U_configBinding) HeUBO {
-    int nbFaces;
-    bool colorPerPrimitive;
-    float normalOffset;
-    bool doSkinning;
+    int nbFaces UBODefaultVal(0);
+    bool colorPerPrimitive UBODefaultVal(false);
+    float normalOffset UBODefaultVal(0.0f);
+    bool doSkinning UBODefaultVal(false);
+#ifdef __cplusplus
+    void displayUI(std::string meshName = "") {
+        if (ImGui::CollapsingHeader(("HE UBO " + meshName).c_str())) {
+            ImGui::PushItemWidth(100.0f);
+            ImGui::InputInt("Nb Faces", &nbFaces, 1, 100);
+            ImGui::Checkbox("Color Per Primitive", &colorPerPrimitive);
+            ImGui::SliderFloat("Normal Offset", &normalOffset, 0.001f, 0.1f, "%.3f", ImGuiSliderFlags_Logarithmic);
+            ImGui::Separator();
+        }
+    }
+#endif
 }UBOName(heUbo);
 #endif
 
 #ifdef RESURFACING_PIPELINE
 UBOStruct(scalar, OtherSet, U_configBinding) ResurfacingUBO {
-    int nbFaces;
-    int nbVertices;
-    int elementType;
-    float scaling;
-    bool backFaceCulling;
-    float cullingThreshold;
-    bool doLod;
-    float lodFactor;
-    bool renderMesh;
-    bool hasElementTypeTexture;
+    int nbFaces UBODefaultVal(0);
+    int nbVertices UBODefaultVal(0);
+    int elementType UBODefaultVal(0);
+    float scaling UBODefaultVal(.5f);
+    bool backfaceCulling UBODefaultVal(false);
+    float cullingThreshold UBODefaultVal(.5f);
+    bool doLod UBODefaultVal(false);
+    float lodFactor UBODefaultVal(1.f);
+    bool renderMesh UBODefaultVal(true);
+    bool hasElementTypeTexture UBODefaultVal(false);
 
-    float minorRadius;
-    float majorRadius;
-    vec3 normal1;
-    vec3 normal2;
-    float normalPerturbation;
-    uvec2 MN;
-    uvec2 minResolution;
-    int edgeMode;
-    bool cyclicU;
-    bool cyclicV;
-    uint Nx;
-    uint Ny;
-    uint degree;
-    vec3 minLutExtent;
-    vec3 maxLutExtent;
-    bool doSkinning;
+    float minorRadius UBODefaultVal(.4f);
+    float majorRadius UBODefaultVal(1.f);
+    vec3 normal1 UBODefaultVal(vec3(0,0, 1));
+    vec3 normal2 UBODefaultVal(vec3(0,0, 1));
+    float normalPerturbation UBODefaultVal(0.f);
+    uvec2 MN UBODefaultVal(uvec2(16, 16));
+    uvec2 minResolution UBODefaultVal(uvec2(4, 4));
+    int edgeMode UBODefaultVal(0);
+    bool cyclicU UBODefaultVal(false);
+    bool cyclicV UBODefaultVal(false);
+    uint Nx UBODefaultVal(0);
+    uint Ny UBODefaultVal(0);
+    uint degree UBODefaultVal(3);
+    vec3 minLutExtent UBODefaultVal(vec3(0));
+    vec3 maxLutExtent UBODefaultVal(vec3(0));
+    bool doSkinning UBODefaultVal(false);
+#ifdef __cplusplus
+    void displayUI(std::string meshName = "") {
+        if (ImGui::CollapsingHeader(("Resurfacing UBO " + meshName).c_str())) {
+            ImGui::PushItemWidth(200.0f);
+
+            ImGui::Checkbox("Render Mesh", &renderMesh);
+
+            ImGui::Text("Base Mesh Nb faces: %d", nbFaces);
+            ImGui::Text("Base Mesh Nb vertices: %d", nbVertices);
+            ImGui::Text("Parametric patch : Vertices: %d, Quads %d, Tris%d", (MN.x + 1) * (MN.y + 1), MN.x * MN.y, MN.x * MN.y * 2);
+
+            ImGui::Separator();
+
+            const char *elementTypesNames[] = {"torus", "sphere", "Mobius", "Klein", "Hyperbolic", "Helicoid", "Cone", "Cylinder", "Egg", "B-Spline", "Bezier"};
+            ImGui::Combo("Element Type", &elementType, elementTypesNames, IM_ARRAYSIZE(elementTypesNames));
+            // ImGui::SliderInt("elementType", &elementType, 0, 10);
+
+            ImGui::SliderFloat("Scaling", &scaling, 0.01f, 10, "%.2f", ImGuiSliderFlags_Logarithmic);
+
+            ImGui::Checkbox("Culling", &backfaceCulling);
+            if (backfaceCulling) {
+                ImGui::SameLine();
+                ImGui::SliderFloat("Threshold", &cullingThreshold, 0, 1, "%.2f");
+            }
+
+            ImGui::Checkbox("Do LOD", &doLod);
+            if (doLod) {
+                ImGui::PushItemWidth(50.0f);
+                ImGui::SameLine();
+                ImGui::SliderFloat("lodFactor", &lodFactor, 0, 100, "%.1f", ImGuiSliderFlags_Logarithmic);
+                ImGui::SameLine();
+                ImGui::PopItemWidth();
+                ImGui::PushItemWidth(100.0f);
+                ImGui::SliderInt2("Min Resolution", reinterpret_cast<int *>(&minResolution), 1, 16);
+                ImGui::PopItemWidth();
+            }
+
+            ImGui::Separator();
+
+            ImGui::SliderFloat3("Normal 1", &normal1[0], -1, 1, "%.2f");
+            ImGui::SliderFloat3("Normal 2", &normal2[0], -1, 1, "%.2f");
+            ImGui::SliderFloat("Normal Perturbation", &normalPerturbation, 0, 1, "%.2f");
+
+            ImGui::SliderInt2("Resolution MN", reinterpret_cast<int *>(&MN), 3, 64);
+
+            if (elementType < 9) {
+                ImGui::SliderFloat("Minor Radius", &minorRadius, 0.01f, 10, "%.2f", ImGuiSliderFlags_Logarithmic);
+                ImGui::SliderFloat("Major Radius", &majorRadius, 0.01f, 10, "%.2f", ImGuiSliderFlags_Logarithmic);
+            }
+
+            if (elementType >= 9) {
+                ImGui::Text("Nx: %d, Ny: %d", Nx, Ny);
+                ImGui::Text("MinLutExtent: (%.1f, %.1f, %.1f)", minLutExtent[0], minLutExtent[1], minLutExtent[2]);
+                ImGui::Text("MaxLutExtent: (%.1f, %.1f, %.1f)", maxLutExtent[0], maxLutExtent[1], maxLutExtent[2]);
+                ImGui::Checkbox("Cyclic U", &cyclicU);
+                ImGui::Checkbox("Cyclic V", &cyclicV);
+                if (elementType == 10)
+                    ImGui::SliderInt("Degree", reinterpret_cast<int *>(&degree), 1, 3);
+            }
+
+            ImGui::PopItemWidth();
+            ImGui::Separator();
+        }
+    }
+#endif
 }UBOName(resurfacingUbo);
 #endif
 
 #ifdef PEBBLE_PIPELINE
 UBOStruct(scalar, OtherSet, U_configBinding) PebbleUBO {
-    uint subdivisionLevel;
-    uint subdivOffset;
-    float extrusionAmount;
-    float extrusionVariation;
-    float roundness;
-    uint normalCalculationMethod;
+    uint subdivisionLevel UBODefaultVal(3);
+    uint subdivOffset UBODefaultVal(0);
+    float extrusionAmount UBODefaultVal(0.1f);
+    float extrusionVariation UBODefaultVal(0.5f);
+    float roundness UBODefaultVal(2.0f);
+    uint normalCalculationMethod UBODefaultVal(1);
 
-    float fillradius;
-    float ringoffset;
+    float fillradius UBODefaultVal(0.f);
+    float ringoffset UBODefaultVal(0.3f);
 
-    bool useLod;
-    float lodFactor;
-    bool allowLowLod;
-    uint BoundingBoxType;
+    bool useLod UBODefaultVal(false);
+    float lodFactor UBODefaultVal(1.f);
+    bool allowLowLod UBODefaultVal(false);
+    uint BoundingBoxType UBODefaultVal(0);
 
-    bool useCulling;
-    float cullingThreshold;
+    bool useCulling UBODefaultVal(false);
+    float cullingThreshold UBODefaultVal(0.1f);
 
-    float time;
+    float time UBODefaultVal(0.f);
 
-    bool enableRotation;
-    float rotationSpeed;
-    float scalingThreshold;
+    bool enableRotation UBODefaultVal(false);
+    float rotationSpeed UBODefaultVal(0.1f);
+    float scalingThreshold UBODefaultVal(0.1f);
 
-    bool doNoise;
-    float noiseAmplitude;
-    float noiseFrequency;
-    float normalOffset;
+    bool doNoise UBODefaultVal(false);
+    float noiseAmplitude UBODefaultVal(0.01f);
+    float noiseFrequency UBODefaultVal(50.0f);
+    float normalOffset UBODefaultVal(0.2f);
+#ifdef __cplusplus
+    void displayUI(std::string meshName = "") {
+        if (ImGui::CollapsingHeader(("Pebble UBO " + meshName).c_str())) {
+
+            if (ImGui::SliderInt("Subdivision Level", (int *)&subdivisionLevel, 0, 8)) {
+                subdivOffset = glm::min(subdivOffset, subdivisionLevel);
+            }
+
+            ImGui::SliderInt("Sudivision Offset", (int *)&subdivOffset, 0, glm::min(subdivisionLevel, 3u));
+            ImGui::SliderFloat("Extrusion", &extrusionAmount, 0, 1, "%.3f");
+            ImGui::SliderFloat("Variation", &extrusionVariation, 0, 1, "%.3f");
+            ImGui::SliderFloat("Roundness", &roundness, 0, 2, "%.2f");
+
+            // if (ImGui::CollapsingHeader("Top Face")) {
+            //     ImGui::SliderFloat("Fill Radius", &fillradius, 0, 1, "%.3f");
+            //     ImGui::SliderFloat("Rings offset", &ringoffset, 0, 1, "%.3f");
+            // }
+
+            ImGui::Separator();
+
+            // ImGui::DragFloat("Time", &time, 0.01f, 0, 1000, "%.3f");
+
+            ImGui::Checkbox("Enable Demo", &enableRotation);
+            if (enableRotation) {
+                // ImGui::SliderFloat("Rotation Speed", &rotationSpeed, 0, 10, "%.3f");
+                // ImGui::SliderFloat("Scaling Threshold", &scalingThreshold, 0, 1, "%.3f");
+            }
+
+            ImGui::Checkbox("Do Noise", &doNoise);
+            if (doNoise) {
+                ImGui::SliderFloat("Noise Amplitude", &noiseAmplitude, 0, 1, "%.3f", ImGuiSliderFlags_Logarithmic);
+                ImGui::SliderFloat("Noise Frequency", &noiseFrequency, 0, 100, "%.3f");
+                // ImGui::SliderFloat("Normal Offset", &normalOffset, 0, 1, "%.3f");
+            }
+
+            ImGui::Separator();
+
+            ImGui::Checkbox("Culling", &useCulling);
+            if (useCulling) {
+                ImGui::SameLine();
+                ImGui::SliderFloat("Threshold", &cullingThreshold, 0, 1, "%.2f");
+            }
+
+            ImGui::Checkbox("Use Lod", &useLod);
+            if (useLod) {
+                ImGui::SliderInt("BoundingBoxType", (int *)&BoundingBoxType, 0, 3);
+                ImGui::SliderFloat("LodFactor", &lodFactor, 0, 10, "%.3f");
+                ImGui::Checkbox("Allow Low Lod", &allowLowLod);
+            }
+        }
+    }
+#endif
 }UBOName(pebbleUbo);
 #endif
 
@@ -347,12 +515,12 @@ static vk::DescriptorSetLayout getDescriptorSetLayoutInfo(const int p_set, const
         };
         bindingFlags = {
             {vk::DescriptorBindingFlagBits::eUpdateAfterBind},
-            {vk::DescriptorBindingFlagBits::eUpdateAfterBind| vk::DescriptorBindingFlagBits::ePartiallyBound},
-            {vk::DescriptorBindingFlagBits::eUpdateAfterBind| vk::DescriptorBindingFlagBits::ePartiallyBound},
-            {vk::DescriptorBindingFlagBits::eUpdateAfterBind| vk::DescriptorBindingFlagBits::ePartiallyBound},
-            {vk::DescriptorBindingFlagBits::eUpdateAfterBind| vk::DescriptorBindingFlagBits::ePartiallyBound},
-            {vk::DescriptorBindingFlagBits::eUpdateAfterBind| vk::DescriptorBindingFlagBits::ePartiallyBound},
-            {vk::DescriptorBindingFlagBits::eUpdateAfterBind| vk::DescriptorBindingFlagBits::ePartiallyBound},
+            {vk::DescriptorBindingFlagBits::eUpdateAfterBind | vk::DescriptorBindingFlagBits::ePartiallyBound},
+            {vk::DescriptorBindingFlagBits::eUpdateAfterBind | vk::DescriptorBindingFlagBits::ePartiallyBound},
+            {vk::DescriptorBindingFlagBits::eUpdateAfterBind | vk::DescriptorBindingFlagBits::ePartiallyBound},
+            {vk::DescriptorBindingFlagBits::eUpdateAfterBind | vk::DescriptorBindingFlagBits::ePartiallyBound},
+            {vk::DescriptorBindingFlagBits::eUpdateAfterBind | vk::DescriptorBindingFlagBits::ePartiallyBound},
+            {vk::DescriptorBindingFlagBits::eUpdateAfterBind | vk::DescriptorBindingFlagBits::ePartiallyBound},
         };
     }
     default: ASSERT(false, "Invalid set");
